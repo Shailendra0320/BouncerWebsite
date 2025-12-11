d5d185a084302fa1d6c35e64a3a8fe12959e8a32
Shailendra0320 <145863462+Shailendra0320@users.noreply.github.com>
2025-12-11 12:49:21 +0530

    first commit

---
+++ Backend/Book-Bouncer-near-you/src/main/java/com/bookbouncer/Book/Bouncer/near/you/BookBouncerNearYouApplication.java
@@ -0,0 +1,13 @@
+package com.bookbouncer.Book.Bouncer.near.you;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookBouncerNearYouApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookBouncerNearYouApplication.class, args);
+	}
+
+}
---
+++ Backend/Book-Bouncer-near-you/src/test/java/com/bookbouncer/Book/Bouncer/near/you/BookBouncerNearYouApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bookbouncer.Book.Bouncer.near.you;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookBouncerNearYouApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
